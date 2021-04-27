d6e4245e73e34b3f6425ad00b0f37dcac5b7b5a4
Yuwono <labspintar@gmail.com>
2021-04-27 20:15:37 +0700

    captures

--- app/src/main/java/com/pintarmedia/kamusfarmasi/DetailIstilah.kt
+++ app/src/main/java/com/pintarmedia/kamusfarmasi/DetailIstilah.kt
@@ -2,13 +2,20 @@ package com.pintarmedia.kamusfarmasi
 
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 import com.pintarmedia.kamusfarmasi.model.Farmasi
 import kotlinx.android.synthetic.main.detail_istilah.*
 
 class DetailIstilah : BaseActivity() {
+    lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_istilah)
+        mAdView = findViewById(R.id.ad_view)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
         val farmasi = intent.extras?.get("farmasi") as Farmasi
         istilahTV.text = farmasi.istilah
         artiTV.text = farmasi.arti
